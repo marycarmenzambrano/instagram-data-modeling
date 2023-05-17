f51c1d5f62e4e74830a8be1c2617a08400ee18e9
Marycarmen Zambrano <zambranomarycarmen48@gmail.com>
2023-05-17 02:34:26 +0000

    se modifica tabla

--- src/models.py
+++ src/models.py
@@ -7,49 +7,41 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+class Follower(Base):
     __tablename__ = 'follower'
-    user_from_id = Column(Integer, primary_key=True)
-    user_to_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(String(250), nullable=False)
+    user_to_id = Column(String(250), nullable=False)
 
-class Address(Base):
+class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    username = Column(String(250))
-    firstname = Column(String(250))
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
     lastname = Column(String(250), nullable=False)
-    email = Column(String(250))
-
-
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-class Address(Base):
-    __tablename__ = 'media'
-    id = Column(Integer, primary_key=True)
-    type = Column(String(250))
-    url = Column(String(250))
-    post_id = 
+    email = Column(String(250), nullable=False)
+    follower_id = Column(Integer, ForeignKey("follower.id"))
+    relacion_follower = relationship(Follower)
     
 
-class Address(Base):
+class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    user_id = 
+    
 
-class Address(Base):
+class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250))
-    author_id = 
-    post_id
+    comment_text = Column(String(250), nullable=False)
 
-    
 
 
     def to_dict(self):
         return {}
 
+
+  person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
 
 try:
     result = render_er(Base, 'diagram.png')
